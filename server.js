19b1e8ab303f95b48b8b14e71fd996db80273583
Nirossan <46507846+Nirossan@users.noreply.github.com>
2019-05-11 11:35:59 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    // eslint-disable-next-line no-console
+    console.log('req = \n', req);
+    res.send('Test CI with Travis');
+});
+
+const server = app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log('app running on port = ', PORT);
+});
+
+// eslint-disable-next-line no-console
+console.log('server = ', server);
+
+module.exports = server;
